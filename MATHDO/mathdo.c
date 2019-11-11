5cc65170f207d79641e384296e87338ca68b86c7
NazymAitbayeva <57069819+NazymAitbayeva@users.noreply.github.com>
2019-11-11 23:54:48 +0600

    Update mathdo.c

--- MATHDO/mathdo.c
+++ MATHDO/mathdo.c
@@ -8,20 +8,20 @@ int main(int argc, char* argv[]){
 	int t = 0;
 	pid_t p1 = fork();
 	if(p1==0){					
-		execlp("python","python","add.py",argv[1],argv[2],NULL);
+		execlp("python","python","addition.py",argv[1],argv[2],NULL);
 		exit(0);
 	}
 	pid_t p2 = fork();
 	if(p2==0){
-execlp("java", "java","java.java",argv[1],argv[2],NULL);
+execlp("java", "java","subtraction.java",argv[1],argv[2],NULL);
 		}
 	pid_t p3 = fork();
 	if(p3==0){
-		execlp("node", "node","node.js",argv[1],argv[2],NULL);	
+		execlp("node", "node","multiplication.js",argv[1],argv[2],NULL);	
 	}
 	pid_t p4 = fork();
 	if(p4==0){
-		execlp("bash", "bash","bash.sh",argv[1],argv[2],NULL);
+		execlp("bash", "bash","division.sh",argv[1],argv[2],NULL);
 	}
 	waitpid(p4,0,0);
 	waitpid(p3,0,0);
